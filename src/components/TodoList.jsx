3fa7477e77ca6bb704a1fe06f13b613f62f199ad
Faheem Hassan <51878457+faheem102@users.noreply.github.com>
2024-10-16 00:15:58 +0200

    moving tasks function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,8 +9,12 @@ function TodoList (){
 
     }
     function addTask(){
-        setTasks([...tasks, newTask]);
+        if(newTask.trim() !== ""){
+            setTasks([...tasks, newTask]);
         setNewTask("");
+
+        }
+        
 
     }
     function deleteTask(index){
@@ -19,9 +23,19 @@ function TodoList (){
 
     }
     function moveTaskUp(index){
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index-1]] = [updatedTasks[index-1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
 
     }
     function moveTaskDown(index){
+        if(index < tasks.length -1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index+1]] = [updatedTasks[index+1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
 
     }
     return(<div>
